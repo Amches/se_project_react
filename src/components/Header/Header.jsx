c0c7a1421116cae802a2aebae11991809f3061a7
Amches2 <dan8363@gmail.com>
2025-03-27 18:07:00 +0200

    initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import "./Header.css";
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.png";
+
+function Header() {
+  return <header> Header </header>;
+}
+
+export default Header;
