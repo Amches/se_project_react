429f06c21ca79676d35a9d7d47204929c95f5327
Amches2 <dan8363@gmail.com>
2025-03-28 23:23:31 +0300

    static without styling

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div>Developed by Daniel Amcheslavskiy</div>2025
+    </footer>
+  );
+}
+
+export default Footer;
